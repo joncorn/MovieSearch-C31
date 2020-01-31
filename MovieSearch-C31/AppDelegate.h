1ee5795ec42f2ad5f3c02258d72a96e3c22b86c9
jon <jdcorn@live.com>
2020-01-31 08:41:28 -0700

    Initial Commit

---
+++ MovieSearch-C31/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieSearch-C31
+//
+//  Created by Jon Corn on 1/31/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MovieSearch-C31/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MovieSearch-C31
+//
+//  Created by Jon Corn on 1/31/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
